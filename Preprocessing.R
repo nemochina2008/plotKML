dfad30331db413a21e46301cb18b071332ab23b2
schee053 <daniel.scheerooren@wur.nl>
2015-11-13 16:28:47 +0100

    Finished preprocessing script. From raw xlsx-file, to clean CSV file + R-objects.

--- Preprocessing.R
+++ Preprocessing.R
@@ -4,69 +4,52 @@
 
 getwd()
 setwd("M:/GeoDataMscThesis")
-dir.create("HvA_RawData")
+dir.create("HvA_RawData") # Mannualy put the raw charge data in this folder. Data is not downloadable. 
 dir.create("HvA_Copy_RawData")
-# Copy the RAW data (which you put there) to the Copy folder before setting it as working directory:
 origindir <- c("M:/GeoDataMscThesis/HvA_RawData")
 targetdir <- c("M:/GeoDataMscThesis/HvA_Copy_RawData")
-filestocopy <- c("exp_201301-62014.xlsx", "exp_201306-62014.xlsx", "rapportage_verbruiksdata 201301 + 201306.xlsx")
-file.copy(from=filestocopy, to=targetdir)
-
+filestocopy <- list.files(origindir, all.files = T)
+file.copy(filestocopy, targetdir)
 setwd("M:/GeoDataMscThesis/HvA_Copy_RawData")
-list.files()
 
 #Download required packages
 install.packages("RCurl")
-install.packages("xlsx")
-# install.packages("gdata")
-
-#Load required libraries
+install.packages("openxlsx")
 library(RCurl)
-# library(gdata)
-library(xlsx)
+library(openxlsx)
 
-# DOESNT WORK YET! Package doens't function >>>> Read xlsx file and save as csv file
-# Nuon201301_201306 <- read.xls("exp_201306-62014.xlsx", sheetNames("Sheet1", perl = "perl"))
-# write.csv(Nuon201301_201306, file = paste(i, "csv", sep = "."), row.names=F)
-
-#Read raw CSV data and create two objects (Nuon/Essent)
-NuonRaw<- read.csv("Nuon201301_201306.csv", header = T, sep=",")
-EssentRaw01<- read.csv("Essent201301.csv", header = T, sep=",")
-EssentRaw06<- read.csv("Essent201306.csv", header = T, sep=",")
-  #Merge ROWS of Essent datasets
+# Read xlsx file and create objects
+NuonRaw <- read.xlsx("M:/GeoDataMscThesis/HvA_Copy_RawData/rapportage_verbruiksdata 201301 + 201306.xlsx", sheet = 1, colNames = TRUE)
+EssentRaw01 <- read.xlsx("M:/GeoDataMscThesis/HvA_Copy_RawData/exp_201301-62014.xlsx.xlsx", sheet = 1, colNames = TRUE)
+EssentRaw06 <- read.xlsx("M:/GeoDataMscThesis/HvA_Copy_RawData/exp_201306-62014.xlsx.xlsx", sheet = 1, colNames = TRUE)
+  #Merge ROWS of Essent datasets Jan+Jun
 EssentRaw <- rbind(EssentRaw01, EssentRaw06)
 
 # Download Charge point dataset 
 download.file("https://api.essent.nl/generic/downloadChargingStations?latitude_low=52.30567123031878&longtitude_low=4.756801078125022&latitude_high=52.43772606594848&longtitude_high=5.086390921875022&format=CSV",destfile="ChargeStations.csv",method="libcurl")
 ChargeStations = read.csv("ChargeStations.csv", header = T, sep=";")
-View(ChargeStations)
 
 # Change colomns names (for merge purposes)
-names(NuonRaw)[7] <- "Street"
-names(NuonRaw)[8] <- "HouseNumber"
-names(NuonRaw)[9] <- "PostalCode"
-names(EssentRaw)[10] <- "Street"
-names(EssentRaw)[11] <- "HouseNumber"
-names(EssentRaw)[12] <- "PostalCode"
+names(NuonRaw$Straat) <- "Street"
+names(NuonRaw$Huisnummer) <- "HouseNumber"
+names(NuonRaw$Postcode) <- "PostalCode"
+names(NuonRaw$Start)<- "BEGIN_LOAD_TIME"
+names(NuonRaw$Eind) <- "END_LOAD_TIME"
+names(NuonRaw$Laadtijd) <- "CHARGETIME"
+names(EssentRaw$STREET) <- "Street"
+names(EssentRaw$HOUSE_NUM1) <- "HouseNumber"
+names(EssentRaw$POST_CODE1) <- "PostalCode"
+names(EssentRaw$ENERGIE) <- "kWh"
 
-# Merge charge points x-y coordinates with raw data
+# Add xy-coordinates of charge point dataset to charge datasets
 nuonXYmerged <- merge(NuonRaw,ChargeStations,by=c("Street","HouseNumber", "PostalCode"))
 essentXYmerged <- merge(EssentRaw,ChargeStations, by=c("Street","HouseNumber", "PostalCode"))
-View(nuonXYmerged)
-View(essentXYmerged)
+
 # Remove unnecessary columns
-Essentkeep <- c("Street", "HouseNumber", "PostalCode", "BEGIN_LOAD_DATE", "END_LOAD_DATE", "END_LOAD_TIME", "CHARGETIME", "CHARGE_DURATION", "METER_READ_BEGIN", "METER_READ_END", "ENERGIE", "Latitude", "Longitude", "Provider")
+Essentkeep <- c("Street", "HouseNumber", "PostalCode", "BEGIN_LOAD_DATE", "END_LOAD_DATE", "BEGIN_LOAD_TIME", "END_LOAD_TIME", "CHARGETIME", "CHARGE_DURATION", "METER_READ_BEGIN", "METER_READ_END", "ENERGIE", "Latitude", "Longitude", "Provider")
 EssentClean <- essentXYmerged[Essentkeep]
 Nuonkeep <- c("Street", "HouseNumber", "PostalCode", "Start", "Eind", "Laadtijd", "kWh", "Latitude", "Longitude", "Provider")
 NuonClean <- nuonXYmerged[Nuonkeep]
-
-# Change colomns names (for similarity purposes)
-names(NuonClean$Start)<- "BEGIN_LOAD_TIME"
-# names(NuonRaw)[8] <- "HouseNumber"
-# names(NuonRaw)[9] <- "PostalCode"
-# names(EssentRaw)[10] <- "Street"
-# names(EssentRaw)[11] <- "HouseNumber"
-# names(EssentRaw)[12] <- "PostalCode"
 
 #Write object to csv file for viewing outside R environment
 write.csv(EssentClean, file = "EssentClean.csv")
